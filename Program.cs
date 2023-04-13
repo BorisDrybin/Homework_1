cd50f16f9c460423d0577dea9d4c5cd63590faad
Boris Drybin <makoh93@gmail.com>
2023-04-13 20:03:42 +0300

    first task added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write(a);
+    Console.Write(" - большее число, ");
+    Console.Write(b);
+    Console.Write(" - меньшее число");
+}
+
+else
+{
+    Console.Write(a);
+    Console.Write(" - меньшее число, ");
+    Console.Write(b);
+    Console.Write(" - большее число");
+}
